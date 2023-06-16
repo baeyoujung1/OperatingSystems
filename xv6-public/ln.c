4df5f47f2a9ccb86991662452552668fabf01cde
2021036835 <baeyojung1@hanyang.ac.kr>
2023-06-16 23:55:26 +0900

    project #3

--- xv6-public/ln.c
+++ xv6-public/ln.c
@@ -5,22 +5,24 @@
 int main(int argc, char *argv[])
 {
   
-  if (argc != 3 && (strcmp(argv[1],"-s")!=0))
-  {
-    printf(2, "Usage: ln old new\n");
-    printf(2, "or\n");
-    printf(2, "Usage for symbolic link: ln -s old new\n");
-    exit();
-  }
-  if (argc == 3)
+ 
+  if (argc == 4 && ((strcmp(argv[1], "-h"))==0))
   {
     if (link(argv[2], argv[3]) < 0)
       printf(2, "link %s %s: failed\n", argv[1], argv[2]);
   }
-  else if (strcmp(argv[2], "-s"))
+  else if ((strcmp(argv[1], "-s")==0))
   {
     if (symlink(argv[2], argv[3]) < 0)
       printf(2, "symlink %s %s: failed\n", argv[2], argv[3]);
+  }
+  else
+  {
+    printf(2,"%d", argc);
+    printf(2, "Usage: ln old new\n");
+    printf(2, "or\n");
+    printf(2, "Usage for symbolic link: ln -s old new\n");
+    exit();
   }
   exit();
 }
--- xv6-public/testc.c
+++ xv6-public/testc.c
@@ -1,12 +1,51 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "fs.h"
+#include "fcntl.h"
 
-int main(int argc, char *argv[])
+int index=0;
+
+void
+create(uint filesize)
 {
-	char *buf=argv[1];
-	int ret_val;
-	ret_val=myfunction(buf);
-	printf(1,"Return value:0x%x\n",ret_val);
-	exit();
-};
+  int i;
+  int fds[30];
+  index++;
+
+  char filename[] = "testFile .txt";
+  filename[8] = (char)('0' + index);
+
+  if((fds[index] = open(filename, O_CREATE | O_RDWR)) < 0) {
+    printf(1, "error\n");
+    exit();
+  }
+
+  const uint fs = filesize / 32;
+  for(i = 0; i < fs; i++) {
+    if(write(fds[index], "aaaaaaaaa\n", 16) != 16) {
+      exit();
+    }
+    if(write(fds[index], "bbbbbbbbb\n", 16) != 16) {
+      exit();
+    }
+  }
+
+  printf(1, "Create file %d \n", index);
+}
+
+void
+create_test(void)
+{
+  create(1 * 1024);
+  create(16 * 1024);
+  create(1 * 1024 * 1024);
+  create(16 * 1024 * 1024);
+}
+
+int
+main(int argc, char *argv[])
+{
+  create_test();
+  exit();
+}
